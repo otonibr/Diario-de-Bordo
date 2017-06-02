20ed0cef8b3e501460354b707d24ba65bdd7f5ca
otoni <otonibr@gmail.com>
2017-06-01 23:54:00 -0300

    Add files via upload

---
+++ HelloGItHub/src/hellogithub/HelloGItHub.java
@@ -0,0 +1,15 @@
+// Arquivo em modo console que imprime uma mensagem na tela
+package hellogithub;
+
+/**
+ *
+ * @author otoni ribeiro - otonibr@gmail.com
+ */
+public class HelloGItHub {
+
+    
+    public static void main(String[] args) {
+        // Mensagem a ser impressa na tela
+        System.out.println("Hello GitHub !");
+    
+}}
